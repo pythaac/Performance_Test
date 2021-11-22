6608be6c9fd28e78c5617694c38673fcb51b0aac
pythaac <pythaac@gmail.com>
2021-11-22 19:19:10 +0900

    test modified

--- Array_vs_LinkedList_for_Queue/src/com/company/Main.java
+++ Array_vs_LinkedList_for_Queue/src/com/company/Main.java
@@ -8,6 +8,7 @@ class Node
     Node(int val)
     {
         this.val = val;
+        this.next = null;
     }
 
     void setNext(Node node)
@@ -21,65 +22,124 @@ class Node
     }
 }
 
-class LinkedListTest
+abstract class Test
 {
     final int max;
-    Node head = new Node(0);
 
-    LinkedListTest(int max)
+    Test(int max)
     {
         this.max = max;
     }
 
-    public void run()
+    abstract void enqueue();
+    abstract void find(int num);
+    abstract void dequeue();
+
+    public void run(int num)
+    {
+        final long start = System.currentTimeMillis();
+
+        final long start_enqueue = System.currentTimeMillis();
+        this.enqueue();
+        final long end_enqueue = System.currentTimeMillis();
+
+        long start_find = System.currentTimeMillis();
+        this.find(num);
+        long end_find = System.currentTimeMillis();
+
+        final long start_dequeue = System.currentTimeMillis();
+        this.dequeue();
+        final long end_dequeue = System.currentTimeMillis();
+
+        final long end = System.currentTimeMillis();
+
+        System.out.println("enqueue : "+ (end_enqueue - start_enqueue)/1000.0);
+        System.out.println("find (" + num + ") : "+ (end_find - start_find)/1000.0);
+        System.out.println("dequeue : "+ (end_dequeue - start_dequeue)/1000.0);
+        System.out.println("total : "+ (end - start)/1000.0);
+    }
+}
+
+class LinkedListTest extends Test
+{
+    Node head = new Node(0);
+
+    LinkedListTest(int max)
+    {
+        super(max);
+        System.out.println("[LinkedListTest]");
+    }
+
+    @Override
+    void enqueue()
     {
         Node crnt = this.head;
-        long start = System.currentTimeMillis();
-
         for(int i=1; i<this.max; i++)
         {
             Node nw = new Node(i);
             crnt.setNext(nw);
             crnt=nw;
         }
+    }
 
+    @Override
+    void find(int num)
+    {
+        Node crnt = this.head;
+        for(int i=0; i<this.max; i++)
+        {
+            if (crnt.val == num) break;
+            crnt = crnt.next;
+        }
+    }
+
+    @Override
+    void dequeue()
+    {
         for(int i=0; i<this.max; i++)
         {
             this.head = this.head.popNode();
         }
-
-        long end = System.currentTimeMillis();
-        System.out.println("LinkedListTest : "+ (end - start)/1000.0);
     }
 }
 
-class ArrayTest
+class ArrayTest extends Test
 {
-    final int max;
     int[] arr;
+    int head = 0, rear = 0;
 
     ArrayTest(int max)
     {
-        this.max = max;
+        super(max);
         this.arr = new int[max];
+        System.out.println("[ArrayTest]");
     }
 
-    public void run()
+    @Override
+    void enqueue()
     {
-        int head = 0, rear = 0;
-        long start = System.currentTimeMillis();
-
         for(int i=0; i<this.max; i++)
         {
-            this.arr[head++] = i;
+            this.arr[this.head++] = i;
         }
+    }
 
+    @Override
+    void find(int num)
+    {
+        for(int i=0; i<this.max; i++)
+        {
+            if (this.arr[i] == num) break;
+        }
+    }
+
+    @Override
+    void dequeue()
+    {
         for(int i=0; i<this.max; i++)
         {
             this.arr[rear++] = 0;
         }
-        long end = System.currentTimeMillis();
-        System.out.println("ArrayTest : "+ (end - start)/1000.0);
     }
 }
 
@@ -89,8 +149,9 @@ public class Main {
         final int max = 10_000_000;
 
         ArrayTest arrTest = new ArrayTest(max);
-        arrTest.run();
+        arrTest.run(max-1);
+        System.out.println();
         LinkedListTest linkTest = new LinkedListTest(max);
-        linkTest.run();
+        linkTest.run(max-1);
     }
 }
